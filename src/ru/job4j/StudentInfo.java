87826e15a8fc0a4badcad7e672933addb942982d
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-05-28 11:43:33 +0300

    3.3. Разбор первой программы. [#296172]

---
+++ src/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Stanislav Khabibullin");
+        System.out.println("21.11.1982");
+    }
+}
