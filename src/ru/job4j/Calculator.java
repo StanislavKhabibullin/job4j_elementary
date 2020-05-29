e5310f65ac155ef593d034ae495109e25f1407bf
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-05-29 05:11:45 +0300

    3. Арифметические операции. [#296164]

---
+++ src/ru/job4j/Calculator.java
@@ -0,0 +1,11 @@
+package ru.job4j;
+
+public class Calculator {
+    public static void main(String[] args){
+        int first=1;
+        int second=2;
+        int one_plus_two=first+second;
+        System.out.println(one_plus_two);
+
+    }
+}
