75d754de67eb8bf1aeb1ddd89a6f0508eebe27a2
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-05-31 15:47:48 +0300

    9.2. Checkstyle [#296167]

--- src/ru/job4j/condition/Greeting.java
+++ src/ru/job4j/condition/Greeting.java
@@ -1,12 +1,14 @@
 package ru.job4j.condition;
 
 public class Greeting {
-    public static void main(String[] args){
-        String idea="I like Java!";
+    @SuppressWarnings({"checkstyle:WhitespaceAround", "checkstyle:LeftCurly"})
+    public static void main(String[] args)    {
+        String idea;
+        idea = "I like Java!";
         System.out.println(idea);
-        idea=idea+" But I am a newbie.";
-        int year=2020;
-        idea=idea+" "+year;
+        idea = String.format("%s But I am a newbie.", idea);
+        int year = 2020;
+        idea = String.format("%s %d", idea, year);
         System.out.println(idea);
     }
 }
