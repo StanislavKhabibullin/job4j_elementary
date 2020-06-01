be371a7f02c7db941d9a53bc50b8bb3fae2400aa
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-01 07:28:37 +0300

    0. Что такое тестирование. [#296188]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,28 @@
 package ru.job4j.converter;
 
 public class Converter {
-    public static float rubleToEvro(float value){
-        float rsl=value/78;
+    @SuppressWarnings("checkstyle:WhitespaceAround")
+    public static float rubleToEvro(float value) {
+        float rsl = value / 80;
         return rsl;
     }
-    public static float rubleToDollar(float value){
-        float rsl=value/71;
+
+    public static float rubleToDollar(float value) {
+        float rsl = value / 70;
         return rsl;
     }
-    public static void main (String[] args){
-        float euro=Converter.rubleToEvro(200);
-        System.out.println("200 Rubles = "+euro+" euro");
-        float dollar=Converter.rubleToDollar(200);
-        System.out.println("200 Rubles = "+dollar+" dollars");
+
+    @SuppressWarnings(value = "checkstyle:MethodParamPad")
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEvro(200);
+        System.out.println("200 Rubles = " + euro + " euro");
+        float dollar = Converter.rubleToDollar(200);
+        System.out.println("200 Rubles = " + dollar + " dollars");
+        int in = 140;
+        int expected = 2;
+        float out = Converter.rubleToDollar(in);
+        boolean passed = expected == out;
+        System.out.println("140 Rubbles = 2, test Result :" + passed);
+
     }
 }
