cf13e1ffeabbe02babf0a31626cd5d0b42b6bc76
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-02 06:44:19 +0300

    4.1. Операторы ветвлений [#296122]

---
+++ src/ru/job4j/condition/AlertDivByZero.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+public class AlertDivByZero {
+    public static void main(String[] args) {
+        possibleDiv(4);
+        possibleDiv(0);
+        possibleDiv(-2);
+    }
+
+    public static void possibleDiv(int in) {
+        if (in == 0) {
+            System.out.println("Divizion by " + in + " Alert division by zero!!!");
+        }
+        if (in < 0) {
+            System.out.println("Divizion by " + in + " Alert division by negative numbers");
+        }
+        if (in > 0) {
+            System.out.println("Divizion by " + in + " Correct divizion");
+        }
+    }
+}
