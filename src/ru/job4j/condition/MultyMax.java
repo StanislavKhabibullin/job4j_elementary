1d81ae4a0dfc6a77c72e2b84848ea9ae852ed5ae
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-03 15:36:01 +0300

    4.4. Максимум из трех чисел [#296121]

---
+++ src/ru/job4j/condition/MultyMax.java
@@ -0,0 +1,15 @@
+package ru.job4j.condition;
+
+@SuppressWarnings("checkstyle:WhitespaceAround")
+public class MultyMax {
+    public static int max(int first, int second, int fird) {
+        int result = first > second ? first : second;
+        result = result > fird ? result : fird;
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int rsl = MultyMax.max(1, 5, 9);
+        System.out.println(" из 1; 5; 9 максимальное значение - " + rsl);
+    }
+}
