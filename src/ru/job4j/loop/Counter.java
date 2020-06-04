c4d8322448bdcc7392b1ba5b440a257759a98cde
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-04 21:06:14 +0300

    5.1. Подсчет суммы чётных чисел в диапазоне [#296211]

--- src/ru/job4j/loop/Counter.java
+++ src/ru/job4j/loop/Counter.java
@@ -9,10 +9,24 @@ public class Counter {
         return result;
     }
 
+    public static int sumByEven(int start, int finish) {
+        int result = 0;
+        for (int index = start; index <= finish; index++) {
+            if (index % 2 == 0) {
+                result = result + index;
+            }
+        }
+        return result;
+    }
+
     public static void main(String[] args) {
         System.out.println("summ from 0 to 5 = " + Counter.sum(0, 5));
         System.out.println("summ from 0 to 10 = " + Counter.sum(0, 10));
         System.out.println("summ from 0 to 5 = " + Counter.sum(3, 8));
         System.out.println("summ from 0 to 5 = " + Counter.sum(1, 1));
+
+        System.out.println(sumByEven(0, 10));
+        System.out.println(sumByEven(3, 8));
+        System.out.println(sumByEven(1, 1));
     }
 }
---
+++ test/ru/job4j/loop/CounterTest.java
@@ -0,0 +1,25 @@
+package ru.job4j.loop;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.*;
+
+public class CounterTest {
+
+    @Test
+    public void sumByEvenWhenNumbersFromOneToTenThenThirti() {
+        assertThat(
+                Counter.sumByEven(1, 10),
+                is(30)
+        );
+    }
+
+    @Test
+    public void sumWhenNumbersFromOneToFiveThenFifteen() {
+        assertThat(
+                Counter.sum(1, 5),
+                is(15)
+        );
+    }
+}
