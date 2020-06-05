f68cbf9e839f78fca73707dbf46fc05d2cc08fdb
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-05 07:37:40 +0300

    5.4. Протеиновая диета. [#296203]

---
+++ src/ru/job4j/loop/Fitness.java
@@ -0,0 +1,13 @@
+package ru.job4j.loop;
+
+public class Fitness {
+    public static int calc(int ivan, int nikolai) {
+        int count = 0;
+        while (ivan < nikolai) {
+            ivan = ivan * 3;
+            nikolai = nikolai * 2;
+            count++;
+        }
+        return count;
+    }
+}
