e83555438d172d71a7976ab32cad1da311947727
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-07 17:33:55 +0300

    5.8. JavaDoc. Code convention [#296214]

--- src/ru/job4j/converter/Converter.java
+++ src/ru/job4j/converter/Converter.java
@@ -1,18 +1,36 @@
 package ru.job4j.converter;
 
+/**
+ * Converter
+ * convert rubbles to euro & dollars
+ * @author Rikol skhabibullin@yandex.ru
+ */
+
 public class Converter {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
-    public static int rubleToEvro(int value) {
+    /**
+     * convert Rubbles to Euro
+     * @param value - include Rubbles
+     * @return - how much euro you can buy on value Rubbles
+     */
+        public static int rubleToEvro(int value) {
         int rsl = value / 80;
         return rsl;
     }
 
+    /**
+     *
+     * @param value - include Rubbles
+     * @return how much dollar you can buy on value Rubbles
+     */
     public static int rubleToDollar(int value) {
         int rsl = value / 70;
         return rsl;
     }
 
-    @SuppressWarnings(value = "checkstyle:MethodParamPad")
+    /**
+     * main - основной метод в котором происходит выполнение программы и вывод результата в консоль
+     * @param args - понятия не имею для чего это нужно
+     */
     public static void main(String[] args) {
         float euro = Converter.rubleToEvro(200);
         System.out.println("200 Rubles = " + euro + " euro");
