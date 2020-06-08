a241de51e41ddf6a12d5cd0b3944e1ee30dfbdb7
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-08 12:03:33 +0300

    6.0.5. Массивы в тестах. [#296142]

---
+++ test/ru/job4j/array/SquareTest.java
@@ -0,0 +1,32 @@
+package ru.job4j.array;
+
+import org.junit.Test;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import static org.junit.Assert.*;
+
+public class SquareTest {
+
+    @Test
+    public void whenBound3Then014() {
+        int bound = 3;
+        int[] rst = Square.calculated(bound);
+        int[] expected = new int[]{0, 1, 4};
+        assertThat(
+                rst,
+                is(expected)
+        );
+    }
+
+    @Test
+    public void whenBound5Then014916() {
+        int bound = 5;
+        int[] rst = Square.calculated(bound);
+        int[] expected = new int[]{0, 1, 4, 9, 16};
+        assertThat(
+                rst,
+                is(expected)
+        );
+    }
+    }
