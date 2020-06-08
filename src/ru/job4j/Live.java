32926e5be764d9c1c050f9a0cd1db23e1a352870
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-08 17:29:39 +0300

    5.9.1. Генерация циклов в IDEA - iter, itar. [#296210]

--- src/ru/job4j/Live.java
+++ src/ru/job4j/Live.java
@@ -1,10 +1,7 @@
 package ru.job4j;
 
-@SuppressWarnings("checkstyle:EmptyLineSeparator")
 public class Live {
-
-    @SuppressWarnings({"checkstyle:NoWhitespaceBefore", "checkstyle:WhitespaceAfter"})
-    public static void main(String[] args) {
+        public static void main(String[] args) {
         for (int i = 1; i <= 10; i++) {
             System.out.println(i);
         }
