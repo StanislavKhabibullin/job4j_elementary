6dd46b5fe53a74d990d3d36c4335581dfb25501d
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-20 11:16:00 +0300

    5. Аннотация @Override [#296309]

--- src/ru/job4j/ru/job4j/inheritance/HtmlReport.java
+++ src/ru/job4j/ru/job4j/inheritance/HtmlReport.java
@@ -3,6 +3,7 @@ package ru.job4j.ru.job4j.inheritance;
 @SuppressWarnings("checkstyle:EmptyLineSeparator")
 public class HtmlReport extends TechPort {
 
+    @Override
     public String generate(String name, String body) {
         return "<h1>" + name + "</h1>"
                 + "<br/>"
--- src/ru/job4j/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/ru/job4j/inheritance/JSONReport.java
@@ -1,7 +1,9 @@
 package ru.job4j.ru.job4j.inheritance;
 
 @SuppressWarnings("checkstyle:WhitespaceAround")
-public class JSONReport {
+public class JSONReport extends TechPort {
+
+    @Override
     public String generate(String name, String body) {
         return "name: " + name
                 + System.lineSeparator()
