ac694d5f913f3d22199ede846a33b38094d03c3d
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-20 12:07:40 +0300

    0. Что такое инкапсуляция. [#296321]

---
+++ src/ru/job4j/ru/job4j/encapsulation/ATM.java
@@ -0,0 +1,11 @@
+package ru.job4j.ru.job4j.encapsulation;
+
+public class ATM {
+    public static void main(String[] args) {
+        Account bill = new Account();
+        bill.addMoney(100);
+        bill.balance = 50;
+        System.out.println(bill.balance);
+    }
+
+}
