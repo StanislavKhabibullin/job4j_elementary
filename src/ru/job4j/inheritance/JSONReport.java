1ef26675ae6a66a4a31ce7ec51e817e67dbf9b76
StanislavKhabibullin <doctor_haus77@mail.ru>
2020-06-21 19:52:22 +0300

    4. Переопределение [#296308]

--- src/ru/job4j/inheritance/JSONReport.java
+++ src/ru/job4j/inheritance/JSONReport.java
@@ -5,7 +5,7 @@ public class JSONReport extends TechPort {
 
     @Override
     public String generate(String name, String body) {
-        return "name: " + name
+        return "name: " + name + ','
                 + System.lineSeparator()
                 + "body: " + body;
     }
